65ce87fc39fc14e30e684adfc737a62a9608fe76
Gabriel de Lisle <gabriel@deepsight.io>
2021-12-01 10:18:24 +0100

    dot files and install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install zsh
+brew install k9s
+brew install helm
+brew install git
+brew install htop
+brew install npm
+brew install azure-cli
+brew install grep
+brew install --cask chromedriver
+brew install lightgbm
+brew install postgresql
+brew install jupyter-notebook
+brew install jq
+brew install nvm
